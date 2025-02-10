749d47307c748145a643970256c7191e9a8c2555
rmalina <yang.yuhan3@northeastern.edu>
2025-02-10 02:42:45 -0800

    initializing structure

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomePage.css';
+
+function Home() {
+    return (
+        <div className="name">
+            <h1>Alina Yang</h1>
+        </div>
+    );
+}
+
+export default Home;
